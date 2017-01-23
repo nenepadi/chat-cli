a856238dd13f8569f9605353a19c6aa446e0399f
Obed Ademang <kizit2012@gmail.com>
2017-01-23 07:21:01 +0000

    Beefed up setup file

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+cp -rf /vagrant/chat-cli/ /home/vagrant/chat-cli
+
 VIRTUALENV_DIR=/home/vagrant/.virtualenvs/chat-cli
-PROJECT_DIR=chat-cli
-VIRTUALENV_NAME=clichat
+PROJECT_DIR=/home/vagrant/chat-cli
+VIRTUALENV_NAME=chat-cli
 
 echo "Installing pip"
 
 curl -s https://bootstrap.pypa.io/get-pip.py | python2.7 > /dev/null 2>&1
 
 echo "Mysql Setup"
-apt-get install debconf-utils -y > /dev/null
 debconf-set-selections <<< "mysql-server mysql-server/root_password password admin"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password admin"
-
-apt-get install mysql-server -y > /dev/null
+apt-get update
+apt-get install -y mysql-server
 
 # Setting up MySQL user and db
 mysql -uroot -padmin -e "CREATE DATABASE IF NOT EXISTS chatcli" >> /home/vagrant/chat-cli/vm_build.log 2>&1
@@ -37,10 +38,10 @@ mysql -uroot -padmin -e "CREATE TABLE IF NOT EXISTS chatcli.messages ( \
 echo 'Installing and configuring virtualenvwrapper...'
 pip install --quiet virtualenvwrapper
 
-# setting up virtualenv & installing requirements 
+# setting up virtualenv & installing requirements
 su - vagrant -c "/usr/local/bin/virtualenv $VIRTUALENV_DIR --python=/usr/bin/python2.7 && \
     echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
-    $VIRTUALENV_DIR/bin/pip install -r $PROJECT_DIR/requirements.txt"
+    $VIRTUALENV_DIR/bin/pip install -r /vagrant/requirements.txt"
 
 printf "export WORKON_HOME=/home/vagrant/.virtualenvs\n" >> /home/vagrant/.bashrc
 printf "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7\n" >> ~vagrant/.bashrc
